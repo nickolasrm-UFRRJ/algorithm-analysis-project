192ca5234d25cd2d4880ebb28dcb95f24b1abd0e
frankoliveira <57226425+frankoliveira@users.noreply.github.com>
2020-12-02 22:28:18 -0300

    Update mcts.py
    Fixed and tested version of the MCTS.

--- mcts.py
+++ mcts.py
@@ -2,72 +2,122 @@
 # To install the anytree module run:
 #    pip install anytree
 
+import time
 from anytree import Node, NodeMixin, RenderTree
 import math
+from copy import copy, deepcopy
+from random import seed
+from random import randint
 
+  
 class BaseTree(object):  # Just an example of a base class
     pass
 
 class Tree(BaseTree, NodeMixin):  # Add Node feature
-    def __init__(self, score_sum, parent=None, children=None):
-        super(Tree, self).__init__()
-        self.score_sum = score_sum
-        self.visit_count = 0
-        self.current_board_stage = None
-        self.parent = parent
-        if children:
-           self.children = children
 
-    def get_avarage(self):
-        return (self.score_sum/self.visit_count)
+    def __init__(self, parent=None, children=None):
+        super(Tree, self).__init__()
+        self.score = 0
+        self.visits = 0
+        self.currente_board = None #stores the state of the game
+        self.parent = parent
+        self.terminal = False #terminal: win, lose, draw
+        #self.player = 0
+        if children:
+            self.children = children
+
+    ################
+    #MCTS FUNCTIONS
+    ################
 
     def ucb1(self):
-        if(self.visit_count == 0):
+
+        if(self.visits == 0):
             return math.inf
-        return (self.get_avarage() + 2*(math.sqrt(math.log10(self.parent.score_sum)/self.visit_count)))
+        return ((self.score/self.visits) + math.sqrt(2 * math.log10(self.parent.visits)/self.visits))
 
     def select_node(self): #choses the best node based on the Upper Confidence Bound (UCB1)
 
-        max = 0.0
+        max = -math.inf
         var = self
+        best_node = None
+
         while var.is_leaf == False:
+
             for node in var.children:
                 if node.ucb1() > max:
                     max = node.ucb1()
-                    var = node
+                    best_node = node
+            var = best_node
+            max = -math.inf
+
         return var
 
-    def back_propagation(self, score): #after a simulation, back propagates the new values
+    def back_propagation(self, score): #after a simulation, back propagates the new values: visit and score
+
         if(self.parent != None):
-            self.parent.score_sum += score
-            self.parent.visit_count += 1
+            self.parent.score += score
+            self.parent.visits += 1
             self.parent.back_propagation(score)
 
-def simulate(self, node): #I.A.'s work: simulate the game to return a stage game with its datas
-    self.node = node
-    return node
+    def expand(self): #I.A.'s work
+        #Must return the possible children (actions) and define which of then is a terminal(end, win, draw)
+        #Deve retornar os filhos (ações) possíveis e informar se são terminais
+        # terminais são estados de vitória ou derrota
+    
+    def simulate(self): #I.A.'s work: simulate the game to return a stage game with its datas
+        
+        #must simulate the game state of the node and return its score
 
-def expand(self, node): #I.A.'s work
-    self.node = node
-    return node
 
-def MCTS(tree):
+    def get_best_child_by_visits(self): #selects the most visited child
 
-    node = tree.select_node()
+        max = 0
+        var = None
+        for node in self.root.children:
+            if node.visits > max:
+                max = node.visits
+                var = node
+        return var
 
-    if node.is_leaf: #the node is a leaf: we need to find out its situation(bad or good)
-        result = simulate(node) #A.I. will simulate and give a score to the node
-        node.back_propagation(result.score_sum)
-        return tree.root
-    #the node has alredy been visited: we can expand it by giving new stages(moves in the game)
-    node.children = expand(node) #expands the current node
-    result = node.select_node() #selects the best node to be simulated
-    result = simulate(result)
-    result.back_propagation(result.score_sum)
-    return tree.root
+    def get_best_child_by_ucb(self): #selectes the child that has the highest UBB value
 
-def generateTree(num_iterations):
-    tree = tree(0)
+        max = -math.inf
+        best_child = None
+
+        for node in self.root.children:
+            if node.ucb1() > max:
+                max = node.ucb1()
+                best_child = node
+
+        return best_child
+
+def MCTS(tree): #creates the MCTS
+    
+    leaf = tree.root.select_node()
+
+    if leaf.visits == 0 or leaf.terminal:
+        leaf.score = leaf.simulate()
+        leaf.visits = 1
+        leaf.back_propagation(leaf.score)
+        return leaf.root
+        
+    leaf.children = leaf.expand()  
+    best_leaf_child = leaf.get_best_child_by_ucb()
+    best_leaf_child.score = best_leaf_child.simulate()
+    best_leaf_child.visits = 1
+    best_leaf_child.back_propagation(best_leaf_child.score)
+
+    return leaf.root
+
+
+
+def get_best_action(board, num_iterations): # Returns the most promissing action in the game
+
+    tree = deepcopy(board)    
+    tree.children = tree.expand()
+
     for _ in range(num_iterations):
-         tree = MCTS(tree)
-    return tree
+        tree = MCTS(tree)
+
+    return tree.get_best_child_by_visits().currente_board
